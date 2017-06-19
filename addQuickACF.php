db48a43c03907ed41b15e53ff88d446f29f1f055
duongcuong96 <duongcuong96@gmail.com>
2017-06-20 03:19:44 +0700

    first version

---
+++ addQuickACF.php
@@ -0,0 +1,88 @@
+<?php
+/*
+  Plugin Name: Make AddQuickTags supports ACF Option page !
+  Plugin URI:
+  Description: This dead simple plugin makes AddQuickTags supports working with ACF Option page, It's add all custom Quick Tags in Text mod  of ACF's WYSISYG Editor
+  Version: 1.0.0
+  Author: CuongDC
+  Author URI: cuongdcdev@gmail.com
+  License: GPLv2
+ */
+
+
+function c_is_acf_options_page(){
+    // check if is ACF Option page
+    global $pagenow;
+    global $current_screen;
+    $rs = (preg_match("/.+acf-options.+/" ,$current_screen->id));
+    return ($rs > 0 && $pagenow == "admin.php" ) ? true : false; 
+}
+
+
+function c_addQuickTags_supports_ACF() {
+    if( !c_is_acf_options_page() ) return; //if not ACF option page, simply do nothing 
+    
+    
+    global $current_screen;
+    $addQuickTagOptionString = "rmnlQuicktagSettings";
+    $options = get_option($addQuickTagOptionString);
+
+    if (empty($options['buttons'])) {
+        $options['buttons'] = '';
+    }
+
+    $options['buttons'] = apply_filters('addquicktag_buttons', $options['buttons']);
+    // hook for filter options
+    $options = apply_filters('addquicktag_options', $options);
+
+    if (!$options) {
+        return NULL;
+    }
+
+    if (1 < count($options['buttons'])) {
+        // sort array by order value
+        $tmp = array();
+        foreach ((array) $options['buttons'] as $order) {
+            if (isset($order['order'])) {
+                $tmp[] = $order['order'];
+            } else {
+                $tmp[] = 0;
+            }
+        }
+        array_multisort($tmp, SORT_ASC, $options['buttons']);
+    }
+    
+    
+    ?>
+    <script type="text/javascript">
+       (function($){
+           $(document).ready(function(){
+                var c_addquicktag_tags = <?php echo json_encode($options); ?>,
+                    c_addquicktag_post_type = <?php echo json_encode($current_screen->id); ?>;
+
+                console.log(c_addquicktag_tags["buttons"].length);
+                for( var i = 0 ; i < c_addquicktag_tags["buttons"].length ; i++  ){
+                var button = c_addquicktag_tags["buttons"][i];    
+                QTags.addButton( 
+                        button["attachment"],
+                        button["text"],
+                        button["start"],
+                        button["end"],
+                        button["access"] ,
+                        button["title"]
+                    );
+                }
+           });
+           
+           
+
+       })(jQuery);
+    </script>
+        
+<?php } 
+
+
+
+
+add_action("admin_footer" , "c_addQuickTags_supports_ACF");
+
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Plugin Name ===
+Contributors: CuongDC
+Tags: AddQuickTag, ACF Pro 
+Requires at least: 3.0 
+Tested up to: 4.8
+Stable tag: pretty-stable
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Donate link: https://www.paypal.me/cuongdc
+
+
+== Description ==
+
+   This dead simple plugin makes AddQuickTags supports working with ACF Option page.
+
+   It's add all custom Quick Tags in Text mode  of ACF's WYSISYG Editor.
+
+
+== Installation ==
+
+Unzip this folder and upload it to your WordPress plugin directory, active it and you are done
+
+== Changelog ==
+
+= 0.0.1 =
+    
+    Alphal version
